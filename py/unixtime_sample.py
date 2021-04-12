6c092056cc30f0519630716509fedc27be58e062
nishioka-n <nishioka.nishio@gmail.com>
2021-04-12 19:00:10 +0900

    mod py file

--- py/unixtime_sample.py
+++ py/unixtime_sample.py
@@ -5,14 +5,14 @@ from datetime import date, datetime, timezone
 import locale
 
 # 日付の指定書式
-DATETIME_FORMAT = "%Y/%m/%d %H:%M:%S"
+DATETIME_FORMAT = "%Y/%m/%d %H:%M:%S%z"
 # 作成する時間
 HOURS = 24
 
 # 設定項目 ここから -----------------------------
 
 # 開始日時（UTC）
-START_DATETIME = '2021/04/06 00:01:23'  # 書式は、上記の DATETIME_FORMAT
+START_DATETIME = '2021/4/5 22:02:45+0000'  # 書式は、上記の DATETIME_FORMAT
 
 # 何分ごとに作成するか
 INTERVAL_MINITES = 5
@@ -36,7 +36,7 @@ def get_unixtime_list(start_ts, term, interval) -> list:
 
     items = []
     for i in range(item_count):
-        items.append(start_ts + i)
+        items.append(start_ts + (i * interval))
 
     return items
 
@@ -54,7 +54,8 @@ def main(args=None):
 
     unixtime_list = get_unixtime_list(start_ts, term, interval)
     for ut in unixtime_list:
-        print(ut)
+        ts = datetime.fromtimestamp(ut, timezone.utc)
+        print(ut, ts)
 
 
 if __name__ == '__main__':
